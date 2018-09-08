9909263c9837838f527c1285ae9471d6c00deee6
Zach Deibert <zachdeibert@gmail.com>
2018-09-08 00:16:57 -0500

    Fixed warning

--- src/main/java/com/github/glowingpotato/fastredstone/FastRedstoneMod.java
+++ src/main/java/com/github/glowingpotato/fastredstone/FastRedstoneMod.java
@@ -1,7 +1,5 @@
 package com.github.glowingpotato.fastredstone;
 
-import org.apache.logging.log4j.Logger;
-
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -13,16 +11,11 @@ public class FastRedstoneMod {
 	public static final String NAME = "Fast Redstone";
 	public static final String VERSION = "1.0";
 
-	private static Logger logger;
-
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		logger = event.getModLog();
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-
 	}
-
 }
