c5b143e0861b49f32e58743bed75a1ff33ddfcae
Zach Deibert <zachdeibert@gmail.com>
2018-09-07 21:00:56 -0500

    Refactored project

--- src/main/java/com/github/glowingpotato/fastredstone/FastRedstoneMod.java
+++ src/main/java/com/github/glowingpotato/fastredstone/FastRedstoneMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.github.glowingpotato.fastredstone;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = FastRedstoneMod.MODID, name = FastRedstoneMod.NAME, version = FastRedstoneMod.VERSION)
+public class FastRedstoneMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "fastredstone";
+    public static final String NAME = "Fast Redstone";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
